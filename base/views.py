08370ab76681ef2676937736630e6a34181f1fd1
Rex Mitchell <nogardjmj@gmail.com>
2021-02-27 14:12:36 -0600

    Working on about and file contents pages

--- base/views.py
+++ base/views.py
@@ -4,39 +4,55 @@ from .forms import IssueSearchForm, IssueCreateForm
 
 # Create your views here.
 def home(request):
-    return render(request, 'base/home.html')
+    context = {
+        'title': 'Home'
+    }
+    return render(request, 'base/home.html', context)
 
 
 def search_results(request):
-    return render(request, 'base/search_results.html')
+    context = {
+        'title': 'Search Results'
+    }
+    return render(request, 'base/search_results.html', context)
 
 
 def project_list(request):
     context = {
-        'issue_count': 123
+        'issue_count': 123,
+        'title': 'Projects',
     }
     return render(request, 'base/project_list.html', context)
 
 
 def project_contents(request):
     context = {
-        'issue_count': 0
+        'issue_count': 0,
+        'title': 'Projects',
     }
     return render(request, 'base/project_contents.html', context)
 
 
 def folder_contents(request):
-    return render(request, 'base/folder_contents.html')
+    context = {
+        'title': 'Projects',
+    }
+    return render(request, 'base/folder_contents.html', context)
 
 
 def file_contents(request):
-    return render(request, 'base/file_contents.html')
+    context = {
+        'title': 'Projects',
+        'issues-present': 3,
+    }
+    return render(request, 'base/file_contents.html', context)
 
 
 def issue_create(request):
     form = IssueCreateForm()
     context = {
         'form': form,
+        'title': 'Issues',
     }
     return render(request, 'base/issue_create.html', context)
 
@@ -44,6 +60,7 @@ def issue_create(request):
 def issue_read(request):
     context = {
         'condition': True,
+        'title': 'Issues',
     }
     return render(request, 'base/issue_read.html', context)
 
@@ -52,21 +69,29 @@ def issue_update(request):
     form = IssueCreateForm()
     context = {
         'form': form,
+        'title': 'Issues',
     }
     return render(request, 'base/issue_update.html', context)
 
 
 def issue_delete(request):
-    return render(request, 'base/issue_delete.html')
+    context = {
+        'title': 'Issues',
+    }
+    return render(request, 'base/issue_delete.html', context)
 
 
 def issue_list(request):
     form = IssueSearchForm()
     context = {
         'form': form,
+        'title': 'Issues',
     }
     return render(request, 'base/issue_list.html', context)
 
 
 def about(request):
-    return render(request, 'base/about.html')
+    context = {
+        'title': 'About',
+    }
+    return render(request, 'base/about.html', context)
